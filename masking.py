feaa1bcb4b0fb4f9865149862fe35d3c82c9bc55
SUBRAT KISHORE DUTTA <subratkishoredutta1234@gmail.com>
2020-09-09 11:52:43 +0530

    Add files via upload

---
+++ masking.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  8 15:28:34 2020
+
+@author: Asus
+"""
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+cv2.namedWindow("Trackbars")
+
+def nothing(x):
+    pass
+
+
+cv2.createTrackbar("L-H","Trackbars",0,179,nothing)
+cv2.createTrackbar("L-S","Trackbars",0,255,nothing)
+cv2.createTrackbar("L-V","Trackbars",0,255,nothing)
+cv2.createTrackbar("U-H","Trackbars",179,179,nothing)
+cv2.createTrackbar("U-S","Trackbars",255,255,nothing)
+cv2.createTrackbar("U-V","Trackbars",255,255,nothing)
+
+cam=cv2.VideoCapture(0)
+
+while True:
+     _,img = cam.read()
+     
+     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+     
+     l_h = cv2.getTrackbarPos("L-H","Trackbars")
+     l_s = cv2.getTrackbarPos("L-S","Trackbars")
+     l_v = cv2.getTrackbarPos("L-V","Trackbars")
+     u_h = cv2.getTrackbarPos("U-H","Trackbars")
+     u_s = cv2.getTrackbarPos("U-S","Trackbars")
+     u_v = cv2.getTrackbarPos("U-V","Trackbars")
+     
+     lower_blue = np.array([l_h,l_s,l_v])
+     upper_blue = np.array([u_h,u_s,u_v])
+     
+     mask = cv2.inRange(hsv, lower_blue, upper_blue)
+     
+     cv2.imshow('img',img)
+     cv2.imshow('mask',mask)
+     #cv2.imwrite('mask.jpg',mask)
+     key = cv2.waitKey(1) & 0xFF
+     if key == 27:
+         break
+
+
+cv2.destroyAllWindows()
+
+
+
+
